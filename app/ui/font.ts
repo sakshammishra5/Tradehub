37d6f584b6c1b7f35e0ea4ea4008d9103ccb0cdd
saksham <saksham7dev@gmail.com>
2025-09-20 12:38:35 +0000

    initial commit

---
+++ app/ui/font.ts
@@ -0,0 +1,11 @@
+import {Poppins,Rubik} from "next/font/google";
+
+export const poppins = Poppins({
+    weight:['400'],
+    subsets:['latin']
+})
+
+export const rubik=Rubik({
+    weight:['400'],
+    subsets:['latin']
+})
